a81b80a2dca61721b67c763aad0c5495bc20c012
Tia Leonard <56228669+leonardtia1@users.noreply.github.com>
2020-03-30 16:27:14 -0500

    Update and_or_3.sh

--- and_or_operators/and_or_3.sh
+++ and_or_operators/and_or_3.sh
@@ -4,11 +4,6 @@
 # Date: October 25 , 2019
 # Modified on:             by:
 
-# Logical OR & AND operations are very useful where multiple conditions are used in our programs (scripts).
-# OR is used between two or multiple conditions. It returns true if any one of conditions returns as true.
-# AND is used between two or multiple conditions. It returns true only if all the conditions returns as true.
-# Logical OR in bash script is used with operator -o or (&&)
-# Logical AND in bash script is used with operator -a or (||)
 
 read -p "Enter First Numeric Value: "   first
 read -p "Enter Second Numeric Value: "  second
