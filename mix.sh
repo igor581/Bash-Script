fbe767068ca8df66704894693e136dc8f73642f1
Tia Leonard <56228669+leonardtia1@users.noreply.github.com>
2020-04-24 14:51:07 -0500

    Update mix.sh

--- mix.sh
+++ mix.sh
@@ -35,7 +35,42 @@ HTTPD: /usr/local/apache2/htdocs/
 NGINX: /usr/share/nginx/html/
 TOMCAT: /usr/local/tomcat/webapps/
 
-}
 
 # docker system prune --all
+
+
+COPY index1.html /usr/share/nginx/html
+COPY index2.html /usr/local/apache2/htdocs
+COPY ./pebble.war /usr/local/tomcat/webapps
+
+
+How To Open A Port In CentOS / RHEL 6?
+$ vim /etc/sysconfig/iptables
+-A INPUT -m state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT
+$ sudo iptables restart
+
+
+How To Open A Port In Ubuntu?
+syntax:
+ufw allow port-number/protocol
+example:
+$ sudo ufw allow 8080/tcp
+$ sudo ufw allow 80/tcp
+Ubuntu reload / restart iptables service command
+$ sudo ufw reload
+
+How To Open A Port In CentOS / RHEL 7 through CLI?
+$ sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+$ sudo firewall-cmd --reload
+
+
+#!/bin/bash
+## How to install iptable on CentOS / RHEL 7?
+systemctl stop firewalld
+systemctl disable firewalld
+systemctl mask --now firewalld
+yum -y install iptables-services
+systemctl start iptables
+
+
 
